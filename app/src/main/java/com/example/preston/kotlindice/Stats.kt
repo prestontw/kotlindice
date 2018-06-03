26e404d8d2b62dcf660cbef05b7d537c288abb50
Preston Tunnell Wilson <preston_tunnell_wilson@brown.edu>
2018-06-03 16:36:41 -0400

    add summary text

--- app/src/main/java/com/example/preston/kotlindice/Stats.kt
+++ app/src/main/java/com/example/preston/kotlindice/Stats.kt
@@ -2,12 +2,16 @@ package com.example.preston.kotlindice
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class Stats : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stats)
-        val counts = intent.extras?.getSerializable(MESSAGE)
+        val counts: HashMap<Int, Int> = intent.extras?.getSerializable(MESSAGE) as HashMap<Int, Int>
+        val total = counts.values.sum()
+        val ideal = idealNumRolls(total)
+        findViewById<TextView>(R.id.report).text = mapsToReport(counts, ideal)
     }
 }
